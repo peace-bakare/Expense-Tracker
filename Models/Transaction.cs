874197423836f63161de4e7bd2da5891981198f5
Peace Bakare <peaceultimatum41@outlook.com>
2023-07-08 15:00:46 +0100

    Add project files.

---
+++ Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        //Specifying Foreign Key
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        public int TransactionAmount { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string? TransactionNote { get; set; }
+
+        public DateTime TransactionDate { get; set; }
+    }
+}
